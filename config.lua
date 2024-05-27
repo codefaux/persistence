dac91774ded4b7fd02db2fc9336f8a1c2dc66681
Code Faux <codefaux3@gmail.com>
2024-05-27 15:25:04 -0700

    Write first real workshop.xml draft, remove commented config.lua entries

--- config.lua
+++ config.lua
@@ -1,19 +1,4 @@
 mod_config = {
-	-- money_saved_on_death = 0.25,
-
-	-- research_wand_price_multiplier = 1,
-	-- research_spell_price_multiplier = 10,
-	-- buy_wand_price_multiplier = 1,
-	-- buy_spell_price_multiplier = 1,
-
-	-- enable_edit_wands_in_lobby = true,
-	-- enable_teleport_back_up = true,
-	-- enable_menu_in_holy_mountain = false,
-	-- reuseable_holy_mountain = false,
-	-- move_lobby_to_spawn = false, -- false -> the lobby location will be relative to the mouse controlls stone; true -> the lobby location will be on the spawn location
-
-	-- always_choose_save_id = -1, -- -1 -> choose manualy; 0 -> choose to not use the mod; 1-5 -> choose saveslot
-
 	default_wands = {
 		{
 			name = "Handgun",

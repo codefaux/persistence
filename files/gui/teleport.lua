800ae38be1caba7f2e6f8e728c2680feb4c14c97
Code Faux <codefaux3@gmail.com>
2024-07-09 15:30:10 -0700

    teleport; great lengths to attempt to find lobby, fall back to guesswork. (re: old-version runs)

--- files/gui/teleport.lua
+++ files/gui/teleport.lua
@@ -2,22 +2,39 @@ if teleport_loaded~=true then
   teleport_open=false;
 
   local function draw_teleport()
-    local teleport_confirmation = false;
+    local teleport_confirmation = 0;
     local x_loc = 275;
     local y_loc = 345;
     active_windows["teleport"] = function(_nid)
       GuiZSetForNextWidget(gui, __layer(1))
-      if teleport_confirmation then
-        GuiColorNextWidgetEnum(gui, COLORS.Yellow);
-        if GuiButton(gui, _nid(), x_loc, y_loc, "Press again to teleport to Lobby") then
-          teleport_back_to_lobby();
-        end
-      else
+      if teleport_confirmation==0 then
         GuiColorNextWidgetEnum(gui, COLORS.Green);
         if GuiButton(gui, _nid(), x_loc, y_loc, "Teleport to Lobby") then
-          teleport_confirmation = true;
+          teleport_confirmation = 1;
         end
         GuiGuideTip(gui, "In Persistence Workshop areas, you can teleport to this run's spawn point.", "(Requires confirmation)");
+      elseif teleport_confirmation==1 then
+        GuiColorNextWidgetEnum(gui, COLORS.Yellow);
+        if GuiButton(gui, _nid(), x_loc, y_loc, "Press again to teleport to Lobby") then
+          if teleport_back_to_lobby() then
+            teleport_confirmation = 0;
+          else
+            teleport_confirmation = -1;
+          end
+        end
+      elseif teleport_confirmation==-1 then
+        GuiColorNextWidgetEnum(gui, COLORS.Red);
+        GuiText(gui, x_loc, y_loc - 9, "(CAUTION)", small_text_scale);
+        GuiTooltip(gui, "Warning: Could not find lobby, must assume location.", "Please be prepared to dig out of rock and soil.");
+        GuiColorNextWidgetEnum(gui, COLORS.Yellow);
+        if GuiButton(gui, _nid(), x_loc, y_loc, "Press again to teleport to Lobby") then
+          if teleport_back_to_lobby() then
+            teleport_confirmation = 0;
+          else
+            teleport_confirmation = -1;
+          end
+        end
+        GuiTooltip(gui, "Warning: Could not find lobby, must assume location.", "Please be prepared to dig out of rock and soil.");
       end
     end;
   end
